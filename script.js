6494e825632d07a67e48b77bc6f8faa5b9a4186c
public-2 <qerfrqrqfq@gmail.com>
2022-08-16 13:59:00 +0500

    misollar

---
+++ script.js
@@ -0,0 +1,415 @@
+"use strict"
+
+
+// swich case, html tanlab olish, querySellector, textContent, va innerHtml, getElementById,o'zgarmas 3 lik swech case
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("0."+i + " kg " + i * 100 + " so'm");
+// }
+
+
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + "juft");
+//     } else {
+//         console.log(i + "toq")
+//     }
+// }
+
+// let day = +prompt('hafta kunlarini kiriting')
+
+// switch (day) {
+//     case +'1':
+//         alert("Dushanba");
+//         break;
+//         case +'2':
+//         alert("seshanba");
+//         break;
+//         case +'3':
+//         alert("chorshanba");
+//         break;
+//         case +'4':
+//         alert("payshanb");
+//         break;
+//         case +'5':
+//         alert("juma");
+//         break;
+//         case +'6':
+//         alert("shanba");
+//         break;
+//         case +'7':
+//         alert("oddix");
+//         break;
+//     default:
+//         alert("bunday hafta kuni yoq")
+// }
+
+
+// let a = +prompt(" necha bal kiritgan bo'lsangiz shuni kiriting");
+
+
+// switch (a) {
+//     case  60:
+//         alert("F");
+//         break;
+//     case  70 :
+//         alert("D");
+//         break;
+//     case 80:
+//         alert("B");
+//         break;
+//     case  90 :
+//         alert("A");
+//         break;
+//     default:
+//         alert("bunday baxo yo'q")
+// }
+
+
+
+// Uyga Vazifa
+
+// Qo'shimcha vazifa
+
+// let dollor = 500;
+// let dollor2 = 250;
+// let yevvro = 120;
+// let summa = dollor2 + dollor;
+// let summa2 = yevvro * 10650.34;
+// let summa3 = summa * 11650.34;
+// let summaAll = summa2 + summa3;
+// let a = +prompt("necha pulingiz bor somda kiriting ");
+// if (a > summaAll) {
+//     document.querySelector(".text").innerHTML = "Oq yo'l"
+// } else {
+//     document.querySelector(".text").innerHTML = "Alisher sizda mablag yetarlimas"
+// }
+
+
+
+
+
+
+
+
+
+// Swech case
+
+// misol 1
+
+
+
+
+
+// let day = +prompt('hafta kunlarini kiriting')
+
+// switch (day) {
+//     case +'1':
+//         alert("Dushanba");
+//         break;
+//         case +'2':
+//         alert("seshanba");
+//         break;
+//         case +'3':
+//         alert("chorshanba");
+//         break;
+//         case +'4':
+//         alert("payshanb");
+//         break;
+//         case +'5':
+//         alert("juma");
+//         break;
+//         case +'6':
+//         alert("shanba");
+//         break;
+//         case +'7':
+//         alert("oddix");
+//         break;
+//     default:
+//         alert("bunday hafta kuni yoq")
+// }
+
+
+
+//misol-2
+
+
+
+// let day = +prompt('Baxoyingizni kiriting')
+
+// switch (day) {
+//     case +'1':
+//         alert("qoniqarsiz");
+//         break;
+//         case +'2':
+//         alert("qoniqarsiz");
+//         break;
+//         case +'3':
+//         alert("yomon");
+//         break;
+//         case +'4':
+//         alert("qoniqarli");
+//         break;
+//         case +'5':
+//         alert("alo");
+//         break;
+//     default:
+//         alert(" bunday baxo yoq")
+// }
+
+
+//misol-3
+
+
+// let day = +prompt('Oyni kiriting')
+
+// switch (day) {
+//     case +'1':
+//         alert("Baxor");
+//         break;
+//     case +'2':
+//         alert("Baxor");
+//         break;
+//     case +'3':
+//         alert("Baxor");
+//         break;
+//     case +'4':
+//         alert("yoz");
+//         break;
+//     case +'5':
+//         alert("yoz");
+//         break;
+//     case 6:
+//         alert("yoz");
+//         break;
+//     case +'4':
+//         alert("kuz");
+//         break;
+//     case +'8':
+//         alert("kuz");
+//         break;
+//     case 9:
+//         alert("kuz");
+//         break;
+//     case +'10':
+//         alert("qish");
+//         break;
+//     case +'11':
+//         alert("qish");
+//         break;
+//     case 12:
+//         alert("qish");
+//         break;
+//     default:
+//         alert(" bunday oy yoq")
+// }
+
+
+
+// misol-4
+
+// let day = +prompt('Oyni kiriting va unda necha kun borligini bilib oling')
+
+// switch (day) {
+//     case +'1':
+//         alert("Baxor 1 oy 30 kun bor");
+//         break;
+//     case +'2':
+//         alert("Baxor 2 oy 31 kun bor");
+//         break;
+//     case +'3':
+//         alert("Baxor 3 oy 30 kun bor");
+//         break;
+//     case +'4':
+//         alert("yoz 1 oy 31 kun bor");
+//         break;
+//     case +'5':
+//         alert("yoz 2 oy 30 kun bor");
+//         break;
+//     case 6:
+//         alert("yoz 3 oy 31 kun bor");
+//         break;
+//     case +'4':
+//         alert("kuz 1 oy 30 kun bor");
+//         break;
+//     case +'8':
+//         alert("kuz 2 oy 31 kun bor");
+//         break;
+//     case 9:
+//         alert("kuz  3 oy 30 kun bor");
+//         break;
+//     case +'10':
+//         alert("qish  1 oy 31 kun bor");
+//         break;
+//     case +'11':
+//         alert("qish  2 oy 30 kun bor");
+//         break;
+//     case 12:
+//         alert("qish  3 oy 28 kun bor");
+//         break;
+//     default:
+//         alert(" bunday oy yoq")
+// }
+
+
+
+//misol-5
+
+// let c = prompt();
+// let a = +prompt("a son kiriting");
+// let b = +prompt("b son kiriting");
+// switch (c) {
+//     case "+":
+//         alert(a + b);
+//         break;
+//         case "-":
+//         alert(a - b);
+//         break;
+//         case "*":
+//         alert(a* b);
+//         break;
+//         case "/":
+//         alert(a / b);
+//         break;
+//     default:
+//         alert("bunday amal yoq")
+// }
+
+
+
+//misol-7
+// let n = +prompt("n son kiriting")
+// for (let i = 0; i <= 10; i++ ) {
+//     document.write(n +"<br>")
+// }
+
+//misol-8
+
+
+// let i = 0;
+// for (i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+// document.write(i + "<br> bu son juft")
+//     } else {
+// document.write(i + "<br> bu son toq")
+//     }
+// }
+
+
+//misol-9
+
+// let ten = +prompt("A son kiriting");
+// let number = 0;
+
+// for (let i = 1; i <= ten; i++) {
+//     let n = 1 / i;
+//     number = number + n;
+// }
+
+// alert(number);
+
+// misol-10
+// let n = 10;
+// let x = 2;
+// for (let i = 1; i <= n; i++){
+//     i%x == 0 ?console.log(i):console.log("");
+// }
+//misol-11
+// let i = 0;
+// for (i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+// document.write("")
+//     } else {
+// document.write(i + "bu son toq")
+//     }
+// }
+// misol-12
+// let i = 0;
+// for (i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         document.write(i+"bu son jufft")
+//     } else {
+//         document.write("")
+//     }
+// }
+
+
+// misol-13
+
+// let k = 10;
+// for (let i = 1; i <= k; i++) {
+//     console.log(`${i} kilo ${i*50000}so'm`)
+// }
+
+
+// misol-14
+
+
+// let sum = 0;
+// let a = 2;
+// let b = 10;
+// for (let i = a; i <= b; i++){
+//     sum += i;
+// }
+
+// misol-15
+
+
+// console.log(sum);
+// misol-15
+// let sum = 1;
+// let a = 2;
+// let b = 10;
+// for (let i = a; i <= b; i++) {
+//     sum *= i;
+// }
+// console.log(sum);
+
+// misol-16
+// let n = 5;
+// let sum = 0;
+// let factoryal = 1;
+// for (let i = 1; i <= n; i++) {
+//     factoryal *= i;
+//     sum += factoryal;
+// }
+// console.log(sum);
+
+// misol-17
+// let n = 3;
+// let sum = 0;
+// let realNumber = 3;
+// let factoryal = 1;
+
+// for (let i = 1; i <= n; i++){
+//     let factoryalDivision = (realNumber * i) / (factoryal * i);
+//     sum += factoryalDivision;
+// }
+// console.log(sum);
+
+// misol18
+// let n = 10;
+// let x = 2;
+// for (let i = 1; i <= n; i++){
+//     i %x ==0 ?console.log(i) : console.log();
+// }
+// misol-19
+// let result = 'x';
+
+// for (let i = 0; i < 11; i++) {
+//     for (let j = 0; j < 11; j++) {
+//         if (i == 0 && i > 0) {
+//             result += '[' + i + ']';
+//         } else if (j == 0 && i > 0) {
+//             resultc += '[' + i + ']';
+//         } else if (j == 0 && i > 0) {
+//             resultc += (i * j) + '';
+//         }
+//     }
+//     result += '\n'
+// }
+// console.log(result);
